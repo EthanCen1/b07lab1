c0fda7879d8c194d33c4b7516d6724a6a6e1e016
EthanCen1 <cenzhicen1207@gmail.com>
2024-09-29 23:33:39 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,15 +2,44 @@ public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+
+		double [] coef1 = {1, 2, 3};
+		int [] exp1 = {0, 4, 6};
+		Polynomial p1 = new Polynomial(coef1, exp1);
+		double [] c2 = {3, -2 , -3};
+		int [] exp2 = {1, 4, 7};
+		Polynomial p2 = new Polynomial(coef1, exp2);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		Polynomial product1 = p1.multiply(p2);
+		System.out.println("Product of p1 and p2 evaluation at x=6: " + product1.evaluate(6));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+
+		double[] coef3 = {4, 3};
+		int[] exp3 = {2, 3};
+		Polynomial p3 = new Polynomial(coef3, exp3);
+		double[] coef4 = {1, -3};
+		int[] exp4 = {0, 2};
+		Polynomial p4 = new Polynomial(coef4, exp4);
+		Polynomial sum34 = p3.add(p4);
+		Polynomial product34 = p3.multiply(p4);
+		System.out.println("sum of p3 and p4 evaluation at x=2: " + sum34.evaluate(2));
+		System.out.println("Product of p3 and p4: " + product34.evaluate(1)); 
+
+
+		double[] coef7 = {1, 1, 5};
+		int[] exp7 = {0, 1, 7};
+		Polynomial p7 = new Polynomial(coef7, exp7);
+		double[] coef8 = {2, 2, 3, 4, -5};
+		int[] exp8 = {0, 1, 4, 6, 7};
+		Polynomial p8 = new Polynomial(coef8, exp8);
+		Polynomial product78 = p7.multiply(p8);
+		Polynomial sum78 = p7.add(p8);
+		System.out.println("sum of p3 and p4 evaluation at x=1: " + sum78.evaluate(1));
+		System.out.println("Product of p7 and p8: " + product78.evaluate(1));
+
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,54 +1,133 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Polynomial{
 	double coef[];
+	int exp[];
 
 	public Polynomial(){
-		coef = new double[1];
-		coef[0] = 0;
+		this.coef = new double[1];
+		this.coef[0] = 0;
+		this.exp = new int[1];
+		this.exp[0] = 0;
 		}
 	
-	public Polynomial(double input[]){
-		coef = new double[input.length];
-		for (int i = 0; i < input.length; i++){
-			this.coef[i] = input[i];
+	public Polynomial(double input_coef[], int input_exp[]){
+		this.coef = new double[input_coef.length];
+		this.exp = new int[input_exp.length];
+		for (int i = 0; i < input_coef.length; i++){
+			this.coef[i] = input_coef[i];
+		}
+		for (int j = 0; j < input_exp.length; j++){
+			this.exp[j] = input_exp[j];
 		}
 	}
 
+	public Polynomial(File f) throws FileNotFoundException{
+ 		Scanner scanner = new Scanner(f);
+ 		String line = scanner.nextLine();
+ 		scanner.close();
+ 		line = line.replace("-", "+-");
+ 		String terms[] = line.split("\\+");
+
+	 	this.coef = new double[terms.length];
+	    this.exp = new int[terms.length];
+
+    	for (int i = 0; i < terms.length; i++) {
+	      String[] coef_exp = terms[i].split("x");
+	      this.coef[i] = Double.parseDouble(coef_exp[0]);
+	      this.exp[i] = (coef_exp.length == 1) ? 0: Integer.parseInt(coef_exp[1]);
+    	}
+    }
+
+    public void saveToFile(String expression) throws IOException {
+    	FileWriter writer = new FileWriter(expression);
+    	String line = "";
+    	for (int i = 0; i < this.coef.length; i++) {
+    		if (i == this.coef.length-1){
+    			line += this.coef[i] + "x" + this.exp[i];
+    		}else{
+    		line += this.coef[i] + "x" + this.exp[i] + "+";
+    		}
+    	}
+    	line = line.replace("+-", "-");
+    	line = line.replace("x0", "");
+       	writer.write(line);
+    	writer.close();
+    }
+
 	public Polynomial add(Polynomial input){
-		int min_length;
-		int max_length;
-		if (this.coef.length >= input.coef.length){
-			max_length = this.coef.length;
-			min_length = input.coef.length;
-		}else{
-			max_length = input.coef.length;
-			min_length = this.coef.length;
+		if (this.exp.length == 0){
+			return new Polynomial(input.coef, input.exp);
 		}
+		if (input.exp.length == 0){
+			return new Polynomial(this.coef, this.exp);
+		}
+		int max_length = this.exp.length + input.exp.length;
+		double new_coef[] = new double [max_length];
+		int new_exp[] = new int [max_length];
+		int count = 0;
 
-		double new_coef[] = new double[max_length];
-
-		for (int i = 0; i < max_length; i++){
-			if (i < min_length){
-				new_coef[i] = this.coef[i] + input.coef[i];
-			}else{
-				if (this.coef.length >= input.coef.length){
-					new_coef[i] = this.coef[i];
-				}else{
-					new_coef[i] = input.coef[i];
+		for (int i = 0; i < this.exp.length; i++){
+			boolean flag = false;
+			for (int j = 0; j < input.exp.length; j++){
+				if (this.exp[i] == input.exp[j]){
+					double sum = this.coef[i] + input.coef[j];
+					if (sum != 0){
+						new_coef[count] = sum;
+						new_exp[count] = this.exp[i];
+						count ++;
+					}
+					flag = true;
+					break;
 				}
+			}
+			if (flag == false){
+				new_coef[count] = this.coef[i];
+				new_exp[count] = this.exp[i];
+				count ++;
 			}
 		}
 
-		Polynomial result = new Polynomial(new_coef);
-		return result;
+		for (int p = 0; p < input.exp.length; p++){
+			 boolean flag = false;
+			 for (int q = 0; q < this.exp.length; q++){
+			 	if (this.exp[q] == input.exp[p]){
+			 		flag = true;
+			 		break;
+			 	}
+			 }
+			 if (flag == false){
+			 	new_coef[count] = input.coef[p];
+			 	new_exp[count] = input.exp[p];
+			 	count ++;
+			 }
+		}
+
+		double return_coef[] = new double[count];
+		int return_exp[] = new int[count];
+
+		for (int i = 0; i < count; i++){
+			return_exp[i] = new_exp[i];
+			return_coef[i] = new_coef[i];
+		}
+
+		return new Polynomial(return_coef, return_exp);
 	}
 
 	public double evaluate(double input){
 		double result = 0;
 
 		for (int i = 0; i<this.coef.length; i++){
-			result += this.coef[i] * Math.pow(input, i);
+			result += this.coef[i] * Math.pow(input, this.exp[i]);
 		}
-
 		return result;
 
 	}
@@ -59,5 +138,52 @@ public class Polynomial{
 		}
 		return false;
 	}
+
+	public Polynomial multiply(Polynomial input){
+		int max_length = this.coef.length * input.coef.length;
+		int new_length = 0;
+		double new_coef[] = new double [max_length];
+		int new_exp[] = new int [max_length];
+		int count = 0;
+
+		for (int i = 0; i < this.exp.length; i++){
+			for (int j = 0; j < input.exp.length; j++){
+				new_coef[count] = this.coef[i] * input.coef[j];
+				new_exp[count] = this.exp[i] + input.exp[j];
+				count++;
+			}
+		}
+
+		for (int i = 0; i < count; i++){
+			for (int j = i+1; j < count; j++){
+				if (new_exp[i] == new_exp[j]){
+					new_coef[i] += new_coef[j];
+					new_coef[j] = 0;
+					new_exp[j] = 0; 
+				}
+			}
+		}
+
+		for (int p = 0; p < count; p++){
+			if (new_coef[p] != 0){
+				new_length ++;
+			}
+		}
+
+		double result_coef[] = new double[new_length];
+		int result_exp[] = new int[new_length];
+		int track = 0;
+		for (int q = 0; q < count; q++){
+			if (new_coef[q] != 0){
+				result_coef[track] = new_coef[q];
+				result_exp[track] = new_exp[q];
+				track++;
+			}
+		}
+
+		return new Polynomial(result_coef, result_exp);
+	}
+
+
 
 }
